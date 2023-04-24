db8d7a20a5ca2cde0c3dae768ba5f0a21ee38961
thiagocsr <76794619+thiagocsr@users.noreply.github.com>
2023-04-24 18:11:30 -0300

    Update listaCompraController.js

--- listaCompra/listaCompraController.js
+++ listaCompra/listaCompraController.js
@@ -9,6 +9,24 @@
             mode: 'dismissible'
         });
         tst.fire();
+
+        var registroCriado = event.getParams();
+        alert(registroCriado.response.id);
+        var navService = component.find("navService");
+        var pageReference = {
+            "type": 'standard_recordPage',
+            "attributes": {
+                "recordId": registroCriado.response.id,
+                "actionName": "view",
+                "objectApiName": "Compra_Escritorio__c"
+            }       
+        };
+
+        component.set("v.pageReference", pageReference);
+        
+        var redirect = component.get("v.pageReference");
+        alert(redirect);
+        navService.navigate(redirect);
 },
     handleReset: function(cmp, event, helper){
     cmp.find('field').forEach(function(f){
